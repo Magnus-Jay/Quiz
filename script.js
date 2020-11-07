a438196d02aaea5790b53c6b639d8eac73d17ea1
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-11-07 11:28:47 -0800

    added back in styling and score functionality

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+var questionsRemaining = 5;
+var correctAnswer= "";
+var quizScore = 0;
+var secondsLeft = 30;
+var questionNum = 0;
+var listNum = 1;
+var questionText = $(".questionArea");
+var answerBtnOne = $("#answerOneBtn");
+var answerBtnTwo = $("#answerTwoBtn");
+var answerBtnThree = $("#answerThreeBtn");
+var answerBtnFour = $("#answerFourBtn");
+var highScoreList = $("#high-score-list");
+
+
+
+var startGameBlock = $(".startGame") 
+var questionBlock = $(".questions"); 
+var answersBlock = $(".answers"); 
+var submitBtn = $(".submitArea"); 
+var timeRemaining = $(".timeRemaining"); 
+var gameOver = $(".endGame"); 
+var userScore = $(".score");
+var userName = $("#userName");
+
 //Quiz questions
 const quizQuestions = [
 
@@ -32,51 +57,34 @@ const quizQuestions = [
     
     
 ]
-
-var questionsRemaining = 5;
-var correctAnswer= "";
-var quizScore = 0;
-var secondsLeft = 30;
-var questionNum = 0;
-var listNum = 1;
-var questionText = $(".questionArea");
-var answerBtnOne = $("#answerOneBtn");
-var answerBtnTwo = $("#answerTwoBtn");
-var answerBtnThree = $("#answerThreeBtn");
-var answerBtnFour = $("#answerFourBtn");
-var highScoreList = $("#high-score-list");
-
-
-var startGameBlock = $(".startGame") 
-var questionBlock = $(".questions"); 
-var answersBlock = $(".answers"); 
-var submitBtn = $(".submitArea"); 
-var timeRemaining = $(".timeRemaining"); 
-var gameOver = $(".endGame"); 
-var userScore = $(".score");
-var userName = $("#userName");
-
-$("#startGameBtn").addEventListener("click",function() {
-    startGameBlock.style.display = "none";
-    questionBlock.style.display = "block";
-    answersBlock.style.display = "block";
-    timeRemaining.style.display = "block";
+$("#startGameBtn").on("click",function() {
+    $(".startGame").css.display = "none";
+    questionBlock.css.display = "block";
+    answersBlock.css.display = "block";
+    timeRemaining.css.display = "block";
     timer();
     questionBank();
 })
 
 
-// function timer() {
-//     var timerInterval = setInterval(function() {
-//       secondsLeft--;
-//       timeRemaining.textContent = "You have " + secondsLeft + " seconds remaining";
-//       if(secondsLeft <= 0) {
-//         endTheGame(timerInterval)
-//       }
-//     }, 1000);
-//   }
+function timer() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeRemaining.textContent = "You have " + secondsLeft + " seconds remaining";
+      if(secondsLeft <= 0) {
+        endTheGame(timerInterval)
+      }
+    }, 1000);
+  }
 
 
+function endTheGame(timeoVer) {
+    clearInterval(timeoVer);
+    questionBlock.style.display = "none";
+    answersBlock.style.display = "none";
+    timeRemaining.style.display = "none";
+    gameOver.style.display = "block";
+  }
 
 
 
@@ -88,11 +96,47 @@ function questionBank() {
     answerBtnThree.textContent = quizQuestions[questionNum].answer3;
 }
 
+$(".answers").on("click", function(event) {
 
-function endTheGame(timeoVer) {
-    clearInterval(timeoVer);
-    questionBlock.style.display = "none";
-    answersBlock.style.display = "none";
-    timeRemaining.style.display = "none";
-    gameOver.style.display = "block";
+    if(event.target.matches("button")){
+      var userAnswer = (event.target.innerHTML)
+      var correctAnswer = quizQuestions[questionNum].correctAnswer;
+      questionNum++;
+   
+      if(userAnswer === correctAnswer) {
+        quizScore = quizScore + 20;
+  
+      } else {
+        secondsLeft = secondsLeft - 10;
+      }
+      if(questionNum == 5) {
+        endTheGame(0);
+        return;
+      }
+      questionSelector();
+    };
+  })
+  
+  function renderHighScore() {
+    for (let i = 0; i < listNum; i++) {
+      
+      var li = document.createElement("li");
+      li.setAttribute("data-index", i);
+      li.innerHTML = quizScore + userName.value;
+      highScoreList.appendChild(li);
+    }
   }
+  
+  var highScoreButton = $("#highScoreButton")
+  highScoreButton.on("click", function(event) {
+    localStorage.setItem("Quiz score", quizScore);
+    localStorage.setItem("user name", userName.value);
+    renderHighScore();
+  
+    })
+  
+  
+
+
+
+
